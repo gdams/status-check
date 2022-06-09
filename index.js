ed0367856a4022ec36c52ef63bdcfc0d851edb22
George Adams <georgeadams1995@gmail.com>
2022-06-09 21:03:04 +0100

    Update index.js

--- index.js
+++ index.js
@@ -3,25 +3,23 @@ const github = require('@actions/github');
 
 async function run() {
     try {
-    // Get the JSON webhook payload for the event that triggered the workflow
-    const token = core.getInput('github_token');
-    const octokit = github.getOctokit(token);
-    const payload = github.context.payload
+        const token = core.getInput('github_token');
+        const octokit = github.getOctokit(token);
+        const payload = github.context.payload
 
-    const owner = payload.pull_request.head.repo.owner.login;
-    const repo = payload.pull_request.head.repo.name;
-    const ref = payload.pull_request.head.ref;
+        const owner = payload.pull_request.head.repo.owner.login;
+        const repo = payload.pull_request.head.repo.name;
+        const ref = payload.pull_request.head.ref;
 
-    core.debug(owner, repo, ref)
+        core.setOutput(owner, repo, ref)
 
-    const checks = await octokit.rest.checks.listForRef({
-        owner: owner,
-        repo: repo,
-        ref: ref,
-    });
+        const checks = await octokit.rest.checks.listForRef({
+            owner: owner,
+            repo: repo,
+            ref: ref,
+        });
 
-    core.debug(checks)
-
+        core.setOutput(JSON.stringify(checks))
     } catch (error) {
         core.setFailed(error.message);
     }
