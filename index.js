7172077df73365b050e82919096017678dca379c
George Adams <georgeadams1995@gmail.com>
2022-06-09 20:59:38 +0100

    Update index.js

--- index.js
+++ index.js
@@ -6,6 +6,7 @@ async function run() {
     // Get the JSON webhook payload for the event that triggered the workflow
     const token = core.getInput('github_token');
     const octokit = github.getOctokit(token);
+    const payload = github.context.payload
 
     const owner = payload.pull_request.head.repo.owner.login;
     const repo = payload.pull_request.head.repo.name;
